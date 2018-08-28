1b960e994e04e1a236a04bead0472e0f1da36b8e
Rosa <rosathethompson@gmail.com>
2018-08-28 18:32:42 +1000

    oops

--- starter_code/main.js
+++ starter_code/main.js
@@ -2,6 +2,6 @@ var cities = ['nyc', 'sf', 'la', 'atx', 'syd']
 
 var selectBox = $('#city-type')
 
-cities.forEach(function) {
+cities.forEach(function(city) {
   selectBox.append('<option>' + city + '</option>')
-}
+})
